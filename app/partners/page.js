71a60102594a3d0d94b8eeab1d4c5be1d68ee9e8
dadrockyt-sys <dadrockyt@gmail.com>
2026-06-30 12:57:16 -0500

    Update page.js

--- app/partners/page.js
+++ app/partners/page.js
@@ -1,5 +1,5 @@
 const LOGO_URL = "/dadrock-logo.png";
-const YOUTUBE_URL = "https://youtube.com/@dadrockytofficial?si=TjBWK-QMUu7vdcrI",
+const YOUTUBE_URL = "https://youtube.com/@dadrockytofficial?si=TjBWK-QMUu7vdcrI";
 
 export default function PartnersPage() {
   const stats = [
