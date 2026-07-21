1a01134135bee0fef193a246bdfea814c1f72c30
dadrockyt-sys <dadrockyt@gmail.com>
2026-07-20 21:14:34 -0400

    Update page.js

--- app/[lang]/partners/page.js
+++ app/[lang]/partners/page.js
@@ -1,1 +1,20 @@
+import { locales } from '@/lib/i18n';
+import { notFound } from 'next/navigation';
+import PartnersPage from '../../../partners/page';
 
+export async function generateStaticParams() {
+  return locales
+    .filter((lang) => lang !== 'en')
+    .map((lang) => ({ lang }));
+}
+
+export default async function LocalizedPartnersPage({ params }) {
+  const resolvedParams = await params;
+  const lang = resolvedParams.lang;
+
+  if (!locales.includes(lang) || lang === 'en') {
+    notFound();
+  }
+
+  return <PartnersPage />;
+    }
