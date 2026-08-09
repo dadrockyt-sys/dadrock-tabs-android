72366aef17437611239fb8f5e36b79262e2c747c
dadrockyt-sys <Stephen.s.mcnally@gmail.com>
2026-08-08 19:11:50 -0500

    Add noindex metadata for embed pages

---
+++ app/embed/[slug]/layout.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'DadRock Tabs Embedded Lesson',
+  robots: {
+    index: false,
+    follow: true,
+    googleBot: {
+      index: false,
+      follow: true,
+    },
+  },
+};
+
+export default function EmbedLayout({ children }) {
+  return children;
+}
